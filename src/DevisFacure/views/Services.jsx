accdccfa556dde23f761d21eba9431884fad8f17
Wallin Razafindrazokiny <wallin.razafindrazokiny@gmail.com>
2024-11-20 17:37:00 +0300

    avant push

--- src/DevisFacure/views/Services.jsx
+++ src/DevisFacure/views/Services.jsx
@@ -191,169 +191,113 @@ function Service() {
 
   return (
     <div>
-      <div>
-        <ModalTache isOpen={isModalTacheOpen} onClose={() => setIsModalTacheOpen(false)} />
-      </div>
-      <div>
-        <div className="">
           <div>
-            <nav className="rounded-md flex justify-between items-center p-4 ">
-              <div>
-                <Link className="text-2xl ">
-                  Tous les services
-                </Link>
-              </div>
-            </nav>
-          </div>
+      <ModalTache isOpen={isModalTacheOpen} onClose={() => setIsModalTacheOpen(false)} />
+    </div>
+          <div className="">
+        <div>
+          <nav className="rounded-md flex justify-between items-center p-4 ">
+            <div>
+              <Link className="text-2xl ">
+                Tous les services
+              </Link>
+            </div>
+           </nav>
         </div>
-        <div className="flex flex-wrap">
-        <div className="w-full md:w-10/12 sm:w-10/12">
-          <div className="flex items-center space-x-2">
-            <nav>
-              <button onClick={() => {setIsModalOpen(true); setIsEditModalOpen(false)}} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
-                Nouveau service
-              </button>
-            </nav>
-          </div>
+      </div>
+      <div className="flex flex-wrap">
+      <div className="w-full md:w-10/12 sm:w-10/12">
+      <div className="flex items-center space-x-2">
+        <nav>
+          <button onClick={() => {setIsModalOpen(true); setIsEditModalOpen(false)}} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
+            Nouveau service
+          </button>
+        </nav>
+        </div>
         </div>
 
         <div className="w-full md:w-2/12 sm:w-2/12 ml-auto">
-          <div className="flex mb-4 m-auto py-2">
+            <div className="flex mb-4 m-auto py-2">
             <button 
-              onClick={handlePrev} 
-              disabled={page === 1} 
-              className={`px-4 py-1 border rounded ${page === 1 ? "text-gray-400 cursor-not-allowed" : "text-blue-600"}`}
-            >
-            Prev
-            </button>
-            <button 
-              onClick={handleNext} 
-              disabled={page >= Math.ceil(services.length / itemsPerPage)} 
-              className={`px-4 py-1 border rounded ${page >= Math.ceil(services.length / itemsPerPage) ? "text-gray-400 cursor-not-allowed" : "text-blue-600"}`}
-            >
-              Next
-            </button>
+      onClick={handlePrev} 
+      disabled={page === 1} 
+      className={`px-4 py-1 border rounded ${page === 1 ? "text-gray-400 cursor-not-allowed" : "text-blue-600"}`}
+    >
+      Prev
+    </button>
+    <button 
+      onClick={handleNext} 
+      disabled={page >= Math.ceil(services.length / itemsPerPage)} 
+      className={`px-4 py-1 border rounded ${page >= Math.ceil(services.length / itemsPerPage) ? "text-gray-400 cursor-not-allowed" : "text-blue-600"}`}
+    >
+      Next
+    </button>
+
+            </div>
           </div>
-        </div>
-            
-        </div>
-
-        {/* Table d'affichage des services */}
-        <div className="w-full border rounded-lg shadow-md">
-          <div className="overflow-auto h-[500px]">
-            <table className="min-w-full">
-              <thead className="sticky top-0 bg-white shadow z-1">
-                <tr>
-                  <th className="p-4 text-left text-sm font-medium leading-6">Designation</th>
-                  <th className="p-4 text-left text-sm font-medium leading-6">Description</th>
-                  <th className="p-4 text-left text-sm font-medium leading-6">Tâche</th>
-                  <th className="p-4 text-left text-sm font-medium leading-6">Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {currentServices.map((service, index) => (
-                  <tr key={index} className="hover:bg-gray-100">
-                    <td className="border-y p-4">{service.designation}</td>
-                    <td className="border-y p-4">{service.description}</td>
-                    <td className="border-y p-4">
-                      {service.nombre_tâche}
-                      <button
-                        onClick={() => setIsModalTacheOpen(true)}
-                        className="ml-2 px-2 py-1 text-white bg-blue-500 rounded hover:bg-blue-600"
-                      >
-                        +
-                      </button>
-                    </td>
-                    <td className="border-y p-4">
-                      <div className="relative">
-                        <button
-                          onClick={() =>
-                            setIsOptionsOpen(isOptionsOpen === index ? null : index)
-                          }
-                          className="p-2 hover:bg-gray-200 rounded"
-                        >
-                          <FontAwesomeIcon icon={faEllipsisV} />
-                        </button>
-                        {isOptionsOpen === index && (
-                          <div className="absolute right-0 mt-2 w-32 bg-white border border-gray-200 rounded shadow-md">
-                            <button
-                              onClick={() => {
-                                handleEditService(service);
-                                setIsOptionsOpen(null); // Ferme le menu après l'édition
-                              }}
-                              className="text-blue-500 hover:text-blue-700 flex items-center mb-2 px-2 py-1"
-                            >
-                              <FontAwesomeIcon icon={faEdit} className="mr-2" />
-                              Modifier
-                            </button>
-                            <button
-                              onClick={() => {
-                                handleDeleteService(service.id);
-                                setIsOptionsOpen(null);
-                              }}
-                              className="text-red-500 hover:text-red-700 flex items-center px-2 py-1"
-                            >
-                              <FontAwesomeIcon icon={faTrash} className="mr-2" />
-                              Supprimer
-                            </button>
-                          </div>
-                        )}
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-
-
-          <Modal isOpen={isModalOpen} onClose={() => {setIsModalOpen(false); setErrorMessage("");}}>
-          <h2 className="text-xl">Ajouter un service</h2>
-          <div className="grid grid-cols overflow-y-auto sm:grid-cols-1 max-h-[70vh]">
-          <div className="">
-        
           
-          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-          <form onSubmit={handleSubmit} className="mt-5 grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 gap-4">
-            
-            <div className="mb-4">
-            <label className="block text-sm font-medium">Désignation</label>
-            <input
-                type="text"
-                value={designation}
-                onChange={(e) => setDesignation(e.target.value)}
-                className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-sm font-medium">Description</label>
-              <textarea
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none py-0"
-              />
-            </div>
+      </div>
 
-            <br />
-          <div className="sm:col-span-1 py-2">
-            <button
-              type="submit"
-              className="bg-blue-500 pl-3 pr-3 block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-gray-400 focus:outline-none"
-            >
-              Enregistrer
-            </button>
-          </div></form></div></div>
-        </Modal>
+      {/* Table d'affichage des services */}
+      <div className="w-full border rounded-lg shadow-md overflow-auto h-[500px]">
+      <table className="min-w-full">
+          <thead>
+            <tr>
+              <th className="p-4 text-left text-sm font-medium leading-6">Designation</th>
+              <th className="p-4 text-left text-sm font-medium leading-6">Description</th>
+              <th className="p-4 text-left text-sm font-medium leading-6">Tâche</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentServices.map((service, index) => (
+              <tr key={index}>
+                <td className="border-y p-4">{service.designation}</td>
+                <td className="border-y p-4">{service.description}</td>
+                <td className="border-y p-4">{service.nombre_tâche}<button onClick={() => setIsModalTacheOpen(true)}>+</button></td>
+                 <td className="border-y p-4">
+                  <div className="relative">
+                    <button  onClick={() => setIsOptionsOpen(isOptionsOpen === index ? null : index)}>
+                      <FontAwesomeIcon icon={faEllipsisV} />
+                    </button>
+                    {isOptionsOpen === index && (
+                      <div className="absolute right-0 mt-2 w-32 bg-white border border-gray-200 rounded shadow-md z-10">
+                        <button 
+                    onClick={() => {
+                      handleEditService(service);
+                      setIsOptionsOpen(null); // Ferme le menu après l'édition
+                    }}                          
+                    className="text-blue-500 hover:text-blue-700 flex items-center mb-2">
+                        <FontAwesomeIcon icon={faEdit} className="mr-2" />                          
+                          Modifier
+                        </button>
+                        <button 
+                    onClick={() => {
+                      handleDeleteService(service.id);
+                      setIsOptionsOpen(null);
+                    }}                          
+                    className="text-red-500 hover:text-red-700 flex items-center">
+                        <FontAwesomeIcon icon={faTrash} className="mr-2" />
+                          Supprimer
+                        </button>
+                      </div>
+                    )}
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
 
-
-            <Modal isOpen={isEditModalOpen} onClose={() => {setIsEditModalOpen(false); setServiceToEdit(null)}}>
-        <h2 className="text-lg font-semibold mb-4">Modifier le service</h2>
+        <Modal isOpen={isModalOpen} onClose={() => {setIsModalOpen(false); setErrorMessage("");}}>
+        <h2 className="text-xl">Ajouter un service</h2>
         <div className="grid grid-cols overflow-y-auto sm:grid-cols-1 max-h-[70vh]">
-        <div className="sm:col-span-2">
-
-          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-          <form onSubmit={handleEditSubmit} className="mt-5 grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 gap-4">
+        <div className="">
+       
+        
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        <form onSubmit={handleSubmit} className="mt-5 grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 gap-4">
+          
           <div className="mb-4">
           <label className="block text-sm font-medium">Désignation</label>
           <input
@@ -364,8 +308,8 @@ function Service() {
             />
           </div>
           <div className="mb-4">
-          <label className="block text-sm font-medium">Description</label>
-          <textarea
+            <label className="block text-sm font-medium">Description</label>
+            <textarea
               value={description}
               onChange={(e) => setDescription(e.target.value)}
               className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none py-0"
@@ -373,17 +317,53 @@ function Service() {
           </div>
 
           <br />
-          <div className="sm:col-span-1 py-2">
-            <button
-              type="submit"
-              className="bg-blue-500 pl-3 pr-3 block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-gray-400 focus:outline-none"
-            >
-              Enregistrer
-            </button>
-          </div>
-        </form></div></div>
+        <div className="sm:col-span-1 py-2">
+          <button
+            type="submit"
+            className="bg-blue-500 pl-3 pr-3 block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-gray-400 focus:outline-none"
+          >
+            Enregistrer
+          </button>
+        </div></form></div></div>
       </Modal>
-      </div>
+
+
+          <Modal isOpen={isEditModalOpen} onClose={() => {setIsEditModalOpen(false); setServiceToEdit(null)}}>
+      <h2 className="text-lg font-semibold mb-4">Modifier le service</h2>
+      <div className="grid grid-cols overflow-y-auto sm:grid-cols-1 max-h-[70vh]">
+      <div className="sm:col-span-2">
+
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        <form onSubmit={handleEditSubmit} className="mt-5 grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 gap-4">
+        <div className="mb-4">
+        <label className="block text-sm font-medium">Désignation</label>
+        <input
+            type="text"
+            value={designation}
+            onChange={(e) => setDesignation(e.target.value)}
+            className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none"
+          />
+        </div>
+        <div className="mb-4">
+        <label className="block text-sm font-medium">Description</label>
+        <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none py-0"
+          />
+        </div>
+
+        <br />
+        <div className="sm:col-span-1 py-2">
+          <button
+            type="submit"
+            className="bg-blue-500 pl-3 pr-3 block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-gray-400 focus:outline-none"
+          >
+            Enregistrer
+          </button>
+        </div>
+      </form></div></div>
+    </Modal>
     </div>
   );
 }
